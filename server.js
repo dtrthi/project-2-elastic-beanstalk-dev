229ee60d8218354543ae4e1006fdecfaee414bb6
Dang Truong Thi <thi.dtr@gmail.com>
2024-05-16 22:31:38 +0700

    init the image processing service

---
+++ server.js
@@ -0,0 +1,67 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util.js";
+
+// Init the Express application
+const app = express();
+
+// Set the network port
+const port = process.env.PORT || 8082;
+
+// Use the body parser middleware for post requests
+app.use(bodyParser.json());
+
+// @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+// GET /filteredimage?image_url={{URL}}
+// endpoint to filter an image from a public url.
+// IT SHOULD
+//    1
+//    1. validate the image_url query
+//    2. call filterImageFromURL(image_url) to filter the image
+//    3. send the resulting file in the response
+//    4. deletes any files on the server on finish of the response
+// QUERY PARAMATERS
+//    image_url: URL of a publicly accessible image
+// RETURNS
+//   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+/**************************************************************************** */
+
+//! END @TODO1
+
+// Root Endpoint
+// Displays a simple message to the user
+app.get("/", async (req, res) => {
+  res.send("try GET /filteredimage?image_url={{}}");
+});
+
+app.get("/filteredimage", async (req, res) => {
+  const imageUrl = req.query.image_url;
+  if (typeof imageUrl !== "string" || !isValidHttpUrl(imageUrl)) {
+    return res.status(422).send("invalid image url");
+  }
+
+  const filteredPath = await filterImageFromURL(imageUrl);
+  res.sendFile(filteredPath, () => {
+    deleteLocalFiles([filteredPath]);
+  });
+});
+
+// Start the Server
+app.listen(port, () => {
+  console.log(`server running http://localhost:${port}`);
+  console.log(`press CTRL+C to stop server`);
+});
+
+// from https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+function isValidHttpUrl(string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
